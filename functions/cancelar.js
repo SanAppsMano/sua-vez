75c130173b5a9826adbee351aaa6f58adc1813a0
SanAppsMano <manosandeslimafilho@gmail.com>
2025-05-18 19:27:27 -0300

    Update cancelar.js

--- functions/cancelar.js
+++ functions/cancelar.js
@@ -1,1 +1,13 @@
 
+import { Redis } from "@upstash/redis";
+
+export async function handler(event) {
+  const { clientId } = JSON.parse(event.body);
+  const redis = Redis.fromEnv();
+
+  await redis.del(`ticket:${clientId}`);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ cancelled: true }),
+  };
+}
