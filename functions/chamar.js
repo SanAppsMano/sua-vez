30f2b68f8dce79724f8bf43d909a2038bc29505f
SanAppsMano <manosandeslimafilho@gmail.com>
2025-05-18 19:27:47 -0300

    Update chamar.js

--- functions/chamar.js
+++ functions/chamar.js
@@ -1,1 +1,12 @@
 
+import { Redis } from "@upstash/redis";
+
+export async function handler() {
+  const redis = Redis.fromEnv();
+  const next = await redis.incr("callCounter");
+  await redis.set("currentCall", next);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ called: next }),
+  };
+}
