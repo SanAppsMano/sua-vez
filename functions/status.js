4d7718c5d9a72d5a9156a97ca40ff4e3b7237184
SanAppsMano <manosandeslimafilho@gmail.com>
2025-05-18 20:53:11 -0300

    Update status.js

--- functions/status.js
+++ functions/status.js
@@ -1,13 +1,12 @@
-
 // functions/status.js
 import { Redis } from "@upstash/redis";
 
 export async function handler() {
   const redis = Redis.fromEnv();
-  // Se não houver currentCall definido, retorna 0
-  const current = await redis.get("currentCall") ?? 0;
+  const currentCall = Number(await redis.get("currentCall") || 0);
+  const timestamp   = Number(await redis.get("currentCallTs") || 0);
   return {
     statusCode: 200,
-    body: JSON.stringify({ currentCall: Number(current) }),
+    body: JSON.stringify({ currentCall, timestamp }),
   };
 }
