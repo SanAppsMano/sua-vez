ade3ff235348ea0c429a15e855d60e629691dcb1
SanAppsMano <manosandeslimafilho@gmail.com>
2025-05-18 22:53:38 -0300

    Update cancelados.js

--- functions/cancelados.js
+++ functions/cancelados.js
@@ -3,11 +3,10 @@ import { Redis } from "@upstash/redis";
 
 export async function handler() {
   const redis = Redis.fromEnv();
-  // pega até os 10 últimos cancelamentos
+  // pega os 10 últimos cancelamentos
   const raw = await redis.lrange("cancelledList", 0, 9);
-  // parseia e ordena por ts descendente
   const list = raw
-    .map(str => JSON.parse(str))
+    .map(s => JSON.parse(s))
     .sort((a, b) => b.ts - a.ts);
   return {
     statusCode: 200,
